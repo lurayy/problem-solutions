e7eab444d150c9f625b49a52f87a9240d76740eb
Kabir S. Tamari <kabirtamari@gmail.com>
2022-12-25 20:00:53 +0545

    try

--- two-sum.py
+++ two-sum.py
@@ -1,29 +1,26 @@
 # https://leetcode.com/problems/two-sum/
 
+
 class Solution:
+
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        subs = [target-x for x in nums]
-        print(nums, subs)
-        for index, another in enumerate(subs):
+        for index, value in enumerate(nums):
+            another = target - value
             if another in nums:
-                print(another == nums[nums.index(another)])
-                print([index, nums.index(another)])
-                return [index, nums.index(another)]
+                another_index = nums.index(another)
+                print(another_index, another)
+                if another_index != index:
+                    return [index, another_index]
 
 
-
-cases = (
-    ([2,7,11,15], 9, [0,1]),
-    ([3,2,4], 6, [1,2]),
-    ([3,3], 6, [0,1]),
-    ([0,4,3,0], 0, [0,3])
-)
+cases = (([2, 7, 11, 15], 9, [0, 1]), ([3, 2, 4], 6, [1, 2]),
+         ([3, 3], 6, [0, 1]), ([0, 4, 3, 0], 0, [0, 3]))
 
 sols = Solution()
-for index, case in enumerate( cases):
+for index, case in enumerate(cases):
     if sols.twoSum(case[0], case[1]) == case[2]:
-        # print(f'Case {index} Passed')
+        print(f'Case {index} Passed')
         pass
     else:
         pass
-        # print(f'Case {index} failed')
+        print(f'Case {index} failed')
