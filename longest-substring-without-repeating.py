79428122c3d8b3819514349c6ad0a44f07dc4c76
Kabir S. Tamari <kabirtamari@gmail.com>
2022-12-29 00:13:40 +0545

    started longest substring challenge

---
+++ longest-substring-without-repeating.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+
+class Solution:
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        for alpha in s:
+            pass
